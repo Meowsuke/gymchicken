907921c7558ceabd6f2464bb7e1759e704e12206
Meowsuke <58517864+Meowsuke@users.noreply.github.com>
2025-09-22 17:59:48 +0900

    Initial Commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Shinochan Shop";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An ecommerce plaform where you can buy a lot of things";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
